4958568fce87b2fa00cf2e5e011575520ebc2ef0
saudhasan762 <azanali152@gmail.com>
2021-04-25 15:53:40 +0530

    Added: Initialized stake and bet amount

--- gambler.js
+++ gambler.js
@@ -1,1 +1,7 @@
 console.log("Welcome to gambler simulation problem");
+
+const stake_amount = 100;
+const bet_amount = 1;
+
+console.log("Stake amount for the day", stake_amount);
+console.log("Bet amount for a game", bet_amount);
