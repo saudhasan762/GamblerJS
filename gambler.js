bf0a74cf72eea8c22c01cc2712b73ce4c58962e4
saudhasan762 <azanali152@gmail.com>
2021-04-25 15:59:36 +0530

    Added: Checking if gambler wins or lose

--- gambler.js
+++ gambler.js
@@ -5,3 +5,12 @@ const bet_amount = 1;
 
 console.log("Stake amount for the day", stake_amount);
 console.log("Bet amount for a game", bet_amount);
+
+const getResult = () => {
+    const random = Math.floor(Math.random() * 10) % 2;
+    if(random == 1)
+        console.log("Win");
+    else
+        console.log("Lost");
+}
+getResult();
