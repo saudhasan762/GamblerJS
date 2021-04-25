6282456851528f96683bf684a8a610dfcbfd4844
saudhasan762 <azanali152@gmail.com>
2021-04-25 16:18:31 +0530

    Added: gambler has to resign for a day if he wins or lose by 50%

--- gambler.js
+++ gambler.js
@@ -1,16 +1,25 @@
 console.log("Welcome to gambler simulation problem");
 
-const stake_amount = 100;
+let stake_amount = 100;
 const bet_amount = 1;
 
 console.log("Stake amount for the day", stake_amount);
 console.log("Bet amount for a game", bet_amount);
 
+function getRandom(){
+    return Math.floor(Math.random() * 10) % 2;
+}
+
 const getResult = () => {
-    const random = Math.floor(Math.random() * 10) % 2;
-    if(random == 1)
-        console.log("Win");
-    else
-        console.log("Lost");
+    while (stake_amount > 50 && stake_amount < 150) {
+        if (getRandom() == 1){
+            stake_amount++;
+            console.log("Stake Amount",  stake_amount);
+        } else {
+            stake_amount--;
+            console.log("Stake Amount", stake_amount);
+        }
+    }
+    console.log("Gambler resigned for the day with amount : ", stake_amount);
 }
 getResult();
