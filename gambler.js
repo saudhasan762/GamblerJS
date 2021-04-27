8760f762849ab0f94cb4ca74cc0b1ae47d18f871
saudhasan762 <azanali152@gmail.com>
2021-04-27 07:29:58 +0530

    Added: calculating day wise amount won or lose

--- gambler.js
+++ gambler.js
@@ -6,13 +6,15 @@ const bet_amount = 1;
 //console.log("Stake amount for the day", stake_amount);
 //console.log("Bet amount for a game", bet_amount);
 
-function getRandom() {
-    return Math.floor(Math.random() * 10) % 2;
-}
+// function getRandom() {
+//     return Math.floor(Math.random() * 10) % 2;
+// }
 
+//calculating stake amount for a day
 function calculateAmountForADay(stake_amount) {
     while (stake_amount > 50 && stake_amount < 150) {
-        if (getRandom() == 1) {
+        let random = Math.floor(Math.random() * 10) % 2;
+        if (random == 1) {
             stake_amount++;
             //console.log(" Stake Amount", stake_amount);
         } else {
@@ -23,6 +25,7 @@ function calculateAmountForADay(stake_amount) {
     return stake_amount;
 }
 
+//calculating amount won or lose for 20 days
 function checkWinOrLose(win_amount, lost_amount) {
     let total_amount = 0;
     if (win_amount >= lost_amount) {
@@ -34,16 +37,21 @@ function checkWinOrLose(win_amount, lost_amount) {
     }
 }
 
+//main method
 const getResult = () => {
     let win_amount = 0
     let lost_amount = 0;
     for (let i = 1; i <= 20; i++) {
         stake_amount = 100;
         let amount = calculateAmountForADay(stake_amount);
-        if (amount == 150)
+        if (amount == 150){
+            console.log("Day",i,"Amount Won", amount);
             win_amount = win_amount + 50;
-        else
+    }
+        else{
             lost_amount = lost_amount + 50;
+            console.log("Day",i,"Amount Lost", amount);
+        }
     }
     checkWinOrLose(win_amount, lost_amount);
 }
